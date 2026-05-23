a8c7a5ac0687c976d2069732fb3d1109832258ef
lcolindresj1-hue <lcolindresj1@miumg.edu.gt>
2026-05-23 14:33:49 -0600

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -15,7 +15,6 @@ try {
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    echo "Conexión exitosa a Railway";
 
 } catch (PDOException $e) {
     die("Error de conexión a la base de datos: " . $e->getMessage());
